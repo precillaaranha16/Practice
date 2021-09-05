39d89cf41853365eae7491fec57924538c94f200
Precilla Aranha <precillaaranha16@gmail.com>
2021-09-05 08:17:52 +0530

    Practice

---
+++ src/Test/A.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class A {
+		public static void main(String[] args) {
+			System.out.println("A");	
+			System.out.println("Hi");
+			System.out.println("B");
+		}
+}
---
+++ src/Test/B.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class B {
+public static void main(String[] args) {
+
+	System.out.println("B");
+	System.out.println("A");
+	System.out.println("C");
+}
+}
